c8dae70568e6fdd7a56871cb4cb755312c8c1741
Rafa <rafaelgarciafernandez@Rafaels-MBP.fios-router.home>
2019-04-20 13:47:52 -0400

    Observing dummy LiveData object from activity

--- listofcountries/src/main/java/com/countries/listofcountries/CountryListActivity.kt
+++ listofcountries/src/main/java/com/countries/listofcountries/CountryListActivity.kt
@@ -1,8 +1,10 @@
 package com.countries.listofcountries
 
 import android.os.Bundle
-import com.countries.core.api.CountryApi
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_country_list.*
 import javax.inject.Inject
@@ -10,16 +12,22 @@ import javax.inject.Inject
 class CountryListActivity : DaggerAppCompatActivity() {
 
     @Inject
-    lateinit var countryApi: CountryApi
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    lateinit var viewModel: CountryListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_country_list)
         setSupportActionBar(toolbar)
+        viewModel = ViewModelProviders.of(this, viewModelFactory)[CountryListViewModel::class.java]
+        viewModel.liveData.observe(this, Observer {
+            Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+        })
+    }
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, countryApi.toString(), Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+    override fun onStart() {
+        super.onStart()
+        viewModel.start()
     }
 }
--- listofcountries/src/main/java/com/countries/listofcountries/CountryListViewModel.kt
+++ listofcountries/src/main/java/com/countries/listofcountries/CountryListViewModel.kt
@@ -1,6 +1,23 @@
 package com.countries.listofcountries
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.countries.core.models.Country
 import javax.inject.Inject
 
-class CountryListViewModel @Inject constructor() {
+class CountryListViewModel @Inject constructor(private val useCase: CountryListUseCase) : ViewModel() {
+
+    val liveData = MutableLiveData<Model>(Model.Empty)
+
+    fun start() {
+        //todo add composite
+        useCase.getCountries()
+    }
+
+    sealed class Model {
+        object Empty : Model()
+        object Error : Model()
+        object Loading : Model()
+        data class Content(val countries: List<Country>)
+    }
 }
