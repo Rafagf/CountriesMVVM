e36d46ca42b17f7e85bfc0953b19afb580467b90
Rafa <rafaelgarciafernandez@Rafaels-MBP.fios-router.home>
2019-04-20 13:47:22 -0400

    Explicitly stating returned value type

--- core/src/main/java/com/countries/core/repositories/CountriesRemoteDataSource.kt
+++ core/src/main/java/com/countries/core/repositories/CountriesRemoteDataSource.kt
@@ -1,11 +1,13 @@
 package com.countries.core.repositories
 
 import com.countries.core.api.CountryApi
+import com.countries.core.models.Country
+import io.reactivex.Single
 import javax.inject.Inject
 
 class CountriesRemoteDataSource @Inject constructor(private val api: CountryApi) {
 
-    fun getCountries() = api.getAllCountries()
-    fun getCountryByName(name: String) = api.getCountryByName(name).map { it[0] }
-    fun getCountryByAlpha3(alpha3: String) = api.getCountryByAlpha3(alpha3)
+    fun getCountries(): Single<List<Country>> = api.getAllCountries()
+    fun getCountryByName(name: String): Single<Country> = api.getCountryByName(name).map { it[0] }
+    fun getCountryByAlpha3(alpha3: String): Single<Country> = api.getCountryByAlpha3(alpha3)
 }
