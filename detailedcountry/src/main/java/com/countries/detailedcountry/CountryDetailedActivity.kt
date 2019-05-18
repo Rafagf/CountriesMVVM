f8a85a363650aeacfe0424fd2fd80787cc78cedf
Rafa <rafaelgarciafernandez@Rafaels-MacBook-Pro.local>
2019-05-18 10:54:48 +0100

    Creating detailed country module

---
+++ detailedcountry/src/main/java/com/countries/detailedcountry/CountryDetailedActivity.kt
@@ -0,0 +1,12 @@
+package com.countries.detailedcountry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CountryDetailedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_country_detailed)
+    }
+}
