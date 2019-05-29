c74a145fee4e9cde2ed97df62a4a82be466f5b82
Rafa <rafaelgarciafernandez@Rafaels-MacBook-Pro.local>
2019-05-29 18:48:41 +0100

    Creating generic ErrorView customisable via custom attributes

--- core/src/main/java/com/countries/core/ViewUtils.kt
+++ core/src/main/java/com/countries/core/ViewUtils.kt
@@ -1,6 +1,8 @@
 package com.countries.core
 
 import android.app.Activity
+import android.content.res.TypedArray
+import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.ColorRes
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,6 +21,18 @@ fun View.gone() {
 
 fun View.visible() {
     visibility = View.VISIBLE
+}
+
+fun <T> View.readAttributes(attributeSet: AttributeSet, ids: IntArray, defStyleAttr: Int, block: TypedArray.() -> T): T {
+    fun <T> AttributeSet.read(ids: IntArray, block: TypedArray.() -> T): T {
+        val attributes = context.theme.obtainStyledAttributes(this, ids, defStyleAttr, 0)
+        try {
+            return block(attributes)
+        } finally {
+            attributes.recycle()
+        }
+    }
+    return attributeSet.read(ids, block)
 }
 
 fun LinearLayoutManager.isAtTop() = findFirstCompletelyVisibleItemPosition() == 0
---
+++ core/src/main/java/com/countries/core/views/ErrorView.kt
@@ -0,0 +1,50 @@
+package com.countries.core.views
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.View
+import android.widget.LinearLayout
+import com.countries.core.R
+import com.countries.core.readAttributes
+import kotlinx.android.synthetic.main.error_view.view.*
+
+class ErrorView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    init {
+        View.inflate(context, R.layout.error_view, this)
+        orientation = VERTICAL
+        gravity = Gravity.CENTER
+        attrs.let {
+            readAttributes(it, R.styleable.ErrorView, defStyleAttr) {
+                CustomAttributes(
+                    icon = getDrawable(R.styleable.ErrorView_icon) ?: context.getDrawable(R.drawable.refresh),
+                    title = getString(R.styleable.ErrorView_title) ?: context.getString(R.string.error_header),
+                    message = getString(R.styleable.ErrorView_message) ?: context.getString(R.string.error_body)
+                )
+            }.apply {
+                refreshButton.setImageDrawable(icon)
+                headerTextView.text = title
+                bodyTextView.text = message
+            }
+        }
+    }
+
+    fun onClick(action: () -> Unit) {
+        refreshButton.setOnClickListener {
+            action.invoke()
+        }
+    }
+}
+
+private data class CustomAttributes(
+    val icon: Drawable,
+    val title: String,
+    val message: String
+)
