a313670052101e91bcabe343e1c5f7abc6d90dee
Rafa <rafaelgarciafernandez@Rafaels-MacBook-Pro.local>
2019-06-04 12:28:44 +0100

    Adding slide up animation

--- detailedcountry/src/main/java/com/countries/detailedcountry/CountryDetailedActivity.kt
+++ detailedcountry/src/main/java/com/countries/detailedcountry/CountryDetailedActivity.kt
@@ -2,15 +2,27 @@ package com.countries.detailedcountry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.countries.core.AppNavigator
+import javax.inject.Inject
 
 class CountryDetailedActivity : AppCompatActivity() {
 
-    companion object {
-        const val COUNTRY_NAME_TAG = "name"
-    }
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    @Inject
+    lateinit var navigator: AppNavigator
+
+    lateinit var viewModel: CountryDetailedViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_country_detailed)
+        overridePendingTransition(R.anim.slide_in_up, R.anim.stay)
+    }
+
+    companion object {
+        const val COUNTRY_NAME_TAG = "name"
     }
 }
